c601a0b0a663dfaac561f5311b3c88fa4c144cf1
Natalia <natalia@natalias-mbp.home>
2017-02-26 19:44:31 +0000

    setup test class

---
+++ test/RomanGeneratorTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+import com.bbc.romanconverter.RomanGenerator;
+
+public class RomanGeneratorTest {
+
+    private RomanGenerator converter = new RomanGenerator();
+
+    @Test
+    public void returnsRomanNumerals() throws Exception {
+
+    }
+}
+
