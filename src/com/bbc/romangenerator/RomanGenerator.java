2e25c2072f63b724e06bf0a23eb4b5b26ade0ef8
Natalia <natalia@natalias-mbp.home>
2017-02-27 17:57:44 +0000

    handle caveat

--- src/com/bbc/romangenerator/RomanGenerator.java
+++ src/com/bbc/romangenerator/RomanGenerator.java
@@ -1,12 +1,14 @@
 package com.bbc.romangenerator;
 
-
-public class RomanGenerator implements RomanNumeralGenerator{
-    public String generate (int number) {
+public class RomanGenerator implements RomanNumeralGenerator {
+    public String generate(int number) {
         String roman = "";
 
-        while(number >= 1) {
-            roman +="I";
+        if (number < 1) return "Number must be equal or bigger than 1";
+        if (number > 3999) return "Number must be equal or smaller than 3999";
+
+        while (number >= 1) {
+            roman += "I";
             number--;
         }
         return roman;
--- test/RomanGeneratorTest.java
+++ test/RomanGeneratorTest.java
@@ -7,11 +7,19 @@ public class RomanGeneratorTest {
     private RomanGenerator converter = new RomanGenerator();
 
     @Test
-    public void returnsRomanNumerals() throws Exception {
+    public void returnsRomanNumeralsFromOneToThree() throws Exception {
         assertEquals(converter.generate(1), "I");
         assertEquals(converter.generate(2), "II");
         assertEquals(converter.generate(3), "III");
-        
     }
+
+    @Test
+    public void returnsCaveatMessageIfNumberIsOutOfRange() throws Exception {
+        assertEquals(converter.generate(-5), "Number must be equal or bigger than 1");
+        assertEquals(converter.generate(0), "Number must be equal or bigger than 1");
+        assertEquals(converter.generate(4000), "Number must be equal or smaller than 3999");
+        assertEquals(converter.generate(5087), "Number must be equal or smaller than 3999");
+    }
+
 }
 
