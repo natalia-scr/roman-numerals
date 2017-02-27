db7d27c5a4fbf7d48aa8d781c1306deb2bb28fe9
Natalia <natalia@natalias-mbp.home>
2017-02-27 18:04:38 +0000

    refactor to use LinkedHashMap

--- src/com/bbc/romangenerator/RomanGenerator.java
+++ src/com/bbc/romangenerator/RomanGenerator.java
@@ -1,16 +1,45 @@
 package com.bbc.romangenerator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class RomanGenerator implements RomanNumeralGenerator {
-    public String generate(int number) {
-        String roman = "";
 
+    private LinkedHashMap <Integer, String> numberPairs = new LinkedHashMap <Integer, String>();
+
+    public RomanGenerator() {
+        numberPairs.put(1000, "M");
+        numberPairs.put(900, "CM");
+        numberPairs.put(500, "D");
+        numberPairs.put(400, "CD");
+        numberPairs.put(100, "C");
+        numberPairs.put(90, "XC");
+        numberPairs.put(50, "L");
+        numberPairs.put(40, "XL");
+        numberPairs.put(10, "X");
+        numberPairs.put(9, "IX");
+        numberPairs.put(5, "V");
+        numberPairs.put(4, "IV");
+        numberPairs.put(1, "I");
+    }
+
+
+
+    public String generate (int number) {
         if (number < 1) return "Number must be equal or bigger than 1";
         if (number > 3999) return "Number must be equal or smaller than 3999";
 
-        while (number >= 1) {
-            roman += "I";
-            number--;
+        String roman = "";
+
+        for(Map.Entry<Integer, String> entry : numberPairs.entrySet()) {
+            Integer arabic = entry.getKey();
+            String romanNumeral = entry.getValue();
+
+            while(number >= arabic) {
+                roman += romanNumeral;
+                number -= arabic;
+            }
         }
         return roman;
+
     }
 }
--- test/RomanGeneratorTest.java
+++ test/RomanGeneratorTest.java
@@ -6,6 +6,7 @@ public class RomanGeneratorTest {
 
     private RomanGenerator converter = new RomanGenerator();
 
+    // test roman generator for regular roman numerals
     @Test
     public void returnsRomanNumeralsFromOneToThree() throws Exception {
         assertEquals(converter.generate(1), "I");
@@ -13,12 +14,52 @@ public class RomanGeneratorTest {
         assertEquals(converter.generate(3), "III");
     }
 
+    // test caveat exceptions
     @Test
     public void returnsCaveatMessageIfNumberIsOutOfRange() throws Exception {
         assertEquals(converter.generate(-5), "Number must be equal or bigger than 1");
         assertEquals(converter.generate(0), "Number must be equal or bigger than 1");
         assertEquals(converter.generate(4000), "Number must be equal or smaller than 3999");
         assertEquals(converter.generate(5087), "Number must be equal or smaller than 3999");
+    }
+
+    // test roman generator for irregular roman numerals
+
+    @Test
+    public void returnsCorrectIrregularRomanNumerals() throws Exception {
+        assertEquals(converter.generate(4), "IV");
+        assertEquals(converter.generate(5), "V");
+        assertEquals(converter.generate(9), "IX");
+    }
+
+    // test generator to return correct numerals for numbers that are bigger than 40
+    @Test
+    public void returnsCorrectNumeralsForNumbersBiggerThan40() throws Exception{
+        assertEquals(converter.generate(40), "XL");
+        assertEquals(converter.generate(49), "XLIX");
+        assertEquals(converter.generate(50), "L");
+        assertEquals(converter.generate(55), "LV");
+    }
+
+    // test generator to return correct numerals for numbers that are bigger than 90
+
+    @Test
+    public void returnsCorrectNumeralsForNumbersBiggerThan90() throws Exception {
+        assertEquals(converter.generate(90), "XC");
+        assertEquals(converter.generate(100), "C");
+        assertEquals(converter.generate(400), "CD");
+        assertEquals(converter.generate(500), "D");
+        assertEquals(converter.generate(900), "CM");
+        assertEquals(converter.generate(1000), "M");
+    }
+
+    // test that generator returns the correct numerals for large numbers
+    @Test
+    public void returnsCorrectNumeralsForNumbersBiggerThan1000() throws Exception {
+        assertEquals(converter.generate(1099), "MXCIX");
+        assertEquals(converter.generate(2356), "MMCCCLVI");
+        assertEquals(converter.generate(3789), "MMMDCCLXXXIX");
+        assertEquals(converter.generate(3999), "MMMCMXCIX");
     }
 
 }
